5eea3092b357c03b760c85be7724f89c13f81da5
raffa96 <pocho962010@hotmail.it>
2018-09-25 13:22:02 +0200

    Integrazione modulo mappa migliorato e implementazione del controller per layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import { latLng, tileLayer, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +9,27 @@ import { latLng, tileLayer } from 'leaflet';
 export class AppComponent {
   title = 'angular-material';
 
+  // definizione dei layer di base per la mappa
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+  wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // oggetto controller per i layer
+  layersControl = {
+    baseLayers: {
+      'Street Maps': this.streetMaps,
+      'Wikimedia Maps': this.wMaps
+    }
+  };
+
+  // impostazioni dei layer per inizializzazione della mappa
   options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-      })
-    ],
+    layers: [this.streetMaps],
     zoom: 7,
     center: latLng([46.879966, -121.726909])
   };
